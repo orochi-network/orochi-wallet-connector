1d43f0bcab3b213ce5d12e0dd65f6f61916f995d
Dung Nguyen <dung.nguyen@duelistking.com>
2022-03-08 09:54:04 +0700

    chore: Remove image asset

--- src/wallet-connector/dialog-select-wallet.tsx
+++ src/wallet-connector/dialog-select-wallet.tsx
@@ -1,22 +1,21 @@
-import Avatar from '@mui/material/Avatar';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Dialog from '@mui/material/Dialog';
 import { WalletConnectorContext } from './context';
-import { toCamelCase, toKey } from './utilities';
-import metaMaskIcon from '../static/metamask.png';
-import walletConnectIcon from '../static/walletconnect.png';
+import { toKey } from './utilities';
+// import metaMaskIcon from '../static/metamask.png';
+// import walletConnectIcon from '../static/walletconnect.png';
 
 const supportedWallets = ['MetaMask', 'WalletConnect'];
 
 export function WalletConnectorDialog(props: { onClose: (_connectType: string) => void }) {
-  const images = {
-    metaMask: metaMaskIcon,
-    walletConnect: walletConnectIcon,
-  };
+  // const images = {
+  //   metaMask: metaMaskIcon,
+  //   walletConnect: walletConnectIcon,
+  // };
 
   return (
     <WalletConnectorContext.Consumer>
@@ -27,7 +26,7 @@ export function WalletConnectorDialog(props: { onClose: (_connectType: string) =
             {supportedWallets.map((item) => (
               <ListItem button onClick={() => props.onClose(toKey(item))} key={toKey(item)}>
                 <ListItemAvatar>
-                  <Avatar src={images[toCamelCase(item)]} />
+                  {/* <Avatar src={images[toCamelCase(item)]} /> */}
                 </ListItemAvatar>
                 <ListItemText primary={item} />
               </ListItem>
